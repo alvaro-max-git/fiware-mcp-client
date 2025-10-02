90314682744fd02bc260cb95dff8d80a29763eca
Alvaro Muñoz Pavón <alvaro.munoz.pavon@alumnos.upm.es>
2025-10-02 17:20:52 +0200

    First client version, connects to Fiware MCP server via nkrok but sees no available tools.

--- client.py
+++ client.py
@@ -8,11 +8,30 @@ client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
+MCP_URL = os.getenv("MCP_URL")
+
+tools = [
+    {
+        "type": "mcp",
+        "server_label": "fiware-mcp",
+        "server_url": MCP_URL,
+        # MUY RECOMENDADO: limitar herramientas que verá el modelo
+        "allowed_tools": ["CB_version", "get_all_entities", "get_entity_types"],
+        # para pruebas, ejecuta sin pedir confirmación humana
+        "require_approval": "never",
+    }
+]
+
+prompt = (
+    "Use CB version tool and tell me the Context Broker version."
+)
 
 response = client.responses.create(
     model="gpt-4o-mini",
-    instructions="You are a coding assistant that talks like a pirate.",
-    input="How do I check if a Python object is an instance of a class?",
+    tools=tools,
+    instructions="You are a client that queries a Context Broker through a MCP Server, answering users' questions.",
+    input=prompt,
+    max_output_tokens=800,
 )
 
 print(response.output_text)
